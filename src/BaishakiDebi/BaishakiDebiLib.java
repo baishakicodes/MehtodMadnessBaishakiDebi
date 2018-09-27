79111a2f6053073b25577a60e073fa9a668e1194
BT_1N3_04@BKTECH.LOCAL <BT_1N3_04@BKTECH.LOCAL>
2018-09-27 08:44:08 -0400

    Initial commit

---
+++ src/BaishakiDebi/BaishakiDebiLib.java
@@ -0,0 +1,13 @@
+package BaishakiDebi;
+
+public class BaishakiDebiLib {
+    public static String reverse(String input){
+        String output="";
+        int i=input.length()-1;
+        while(i>=0){
+            output=output + input.substring(i,i+1);
+            i--;
+        }
+        return output;
+    }
+}
---
+++ src/BaishakiDebi/Main.java
@@ -0,0 +1,9 @@
+package BaishakiDebi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("reverse of \'reverse\' is" +BaishakiDebiLib.reverse(input: "reverse"));
+    }
+}
