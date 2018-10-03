536d80962d853bf88aaacb2bc36704231375163b
BT_1N3_04 <baishakicodes>
2018-10-03 08:44:14 -0400

    Fixed the multtable method, it works!!

--- src/BaishakiDebi/BaishakiDebiLib.java
+++ src/BaishakiDebi/BaishakiDebiLib.java
@@ -1,13 +1,13 @@
 package BaishakiDebi;
 
 public class BaishakiDebiLib {
-    public static int multTable(int base, int range){
+    public static void multTable(int base, int range){
         String newString= "";
         for (int i=0; i<range+1; i++){
-            return base*i;
-            newString = newString + base + "*" + i + "=" + (base*i);
+            newString = base + "*" + i + "=" + (base*i);
             System.out.println(newString);
 
         }
     }
+    public static 
 }
