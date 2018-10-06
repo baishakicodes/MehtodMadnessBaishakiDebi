a92220f2b86905a12cb218f6701396cbc49d2f40
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2018-10-06 12:45:19 -0400

    Finished up the methodmadness project.

--- src/BaishakiDebi/BaishakiDebiLib.java
+++ src/BaishakiDebi/BaishakiDebiLib.java
@@ -1,26 +1,63 @@
 package BaishakiDebi;
 
 public class BaishakiDebiLib {
-    public static void multTable(int base, int range){
-        String newString= "";
-        for (int i=0; i<range+1; i++){
-            newString = base + "*" + i + "=" + (base*i);
+    //This method lists the multiplication table for the indicated base up to the indicated range
+    public static void multTable(int base, int range) {
+        String newString = "";
+        for (int i = 0; i < range + 1; i++) {
+            newString = base + "*" + i + "=" + (base * i);
             System.out.println(newString);
 
         }
     }
-    public static int sumUpTo(int num){
-        int sum=0;
-        for(int i=0; i<num+1;i++){
-            sum=sum+i;
+    //This method gives the sum of all the numbers up to the indicated number. Included indicated number in the sum
+    public static int sumUpTo(int num) {
+        int sum = 0;
+        for (int i = 0; i < num + 1; i++) {
+            sum = sum + i;
         }
         return sum;
     }
-    public static String dateStr(String olddate){
-       int i=olddate.indexOf("/");
-       String front = olddate.substring(0,i);
-       String mid = olddate.substring(i+1,i+3);
-       String end = olddate.substring(i+4, olddate.length());
-       return mid+"-"+front+"-"+end;
+    //This method returns the original date in the format mm/dd/yyyy to the format dd-mm-yyyy
+    public static String dateStr(String olddate) {
+        int i = olddate.indexOf("/");
+        String front = olddate.substring(0, i);
+        String mid = olddate.substring(i + 1, i + 3);
+        String end = olddate.substring(i + 4, olddate.length());
+        return mid + "-" + front + "-" + end;
+    }
+    //this method checks in the original word is a palindrome or not. If palindrome it returns true, else false
+    public static Boolean isPalindrome(String word) {
+        String backWord = "";
+        for (int i = word.length(); i > 0; i--) {
+            backWord = backWord + word.substring(i - 1, i);
+        }
+        if (backWord.equals(word)) {
+            return true;
+        }
+        return false;
+    }
+    // this method removes the indicated subStr from the mainStr if subStr is smaller than mainStr.
+    //If subStr is bigger or equal to mainStr the method return a string that statement
+    public static String cutOut(String mainStr, String subStr) {
+        String newString = "";
+        int iSubStr = mainStr.indexOf(subStr);
+        if (mainStr.length() > subStr.length()) {
+            for (int i = 0; i < mainStr.length(); i++) {
+                if (i == iSubStr) {
+                    if (i == 0) {
+                        newString = newString + mainStr.substring(i + subStr.length());
+                    }
+                    if (i > 0) {
+                        newString = newString + mainStr.substring(0, i) + mainStr.substring(i + subStr.length());
+                    }
+
+                }
+            }
+        }
+        else{
+            System.out.println("That subStr is bigger than or equal to the mainStr");
+        }
+        return newString;
     }
 }
--- src/BaishakiDebi/BaishakiDebiMain.java
+++ src/BaishakiDebi/BaishakiDebiMain.java
@@ -1,12 +1,14 @@
 package BaishakiDebi;
 
-public class Main {
+public class BaishakiDebiMain {
 
     public static void main(String[] args) {
 	// write your code here
         BaishakiDebiLib.multTable(9 ,3);
         System.out.println("The sum is "+BaishakiDebiLib.sumUpTo(3));
         System.out.println("The new date is "+BaishakiDebiLib.dateStr("01/08/1999"));
+        System.out.println("Is the word a Palindrome? "+BaishakiDebiLib.isPalindrome("foo"));
+        System.out.println("That subStr cut out from the mainStr gives \'"+BaishakiDebiLib.cutOut("catatonic cat", "cat")+"\'");
 
 
     }
